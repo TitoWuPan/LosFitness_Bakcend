c7d41c6de2633b2f67499ce47fa4990ac68ef624
QuispeAyalaDiego <89178039+QuispeAyalaDiego@users.noreply.github.com>
2022-06-05 20:30:50 -0500

    Add files via upload

---
+++ LosFitness.API/Controllers/AlergiaControler.cs
@@ -0,0 +1,99 @@
+﻿
+using LosFitness.DataAccess;
+using LosFitness.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using static LosFitness.Dto.Request.BaseResponse;
+
+namespace LosFitness.API.Controllers
+{
+
+    [ApiController]
+    [Route("api/[Controller]")]
+
+    public class AlergiaControler:Controller
+    {
+
+        private readonly LosFitnessDbContext _context;
+
+        public AlergiaControler(LosFitnessDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<BaseResponseGeneric<ICollection<Alergia>>>> Get()
+        {
+
+            var response = new BaseResponseGeneric<ICollection<Alergia>>();
+
+            try
+            {
+                response.Result = await _context.Alergias.ToListAsync();
+                response.Success = true;
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                response.Errors.Add(ex.Message);
+                return response;
+
+            }
+
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Alergia>> Get(int id)
+        {
+            var entity = await _context.Alergias.FindAsync(id);
+            if (entity == null)
+            {
+                return NotFound("No se encontró el registro");
+            }
+
+            return Ok(entity);
+        }
+
+        [HttpPost("regsitro/{id:int}")]
+        public async Task<ActionResult> Post(int id, Dto.Request.DtoAlergia request)
+        {
+            var entity = new Alergia
+            {
+                AlimentoId=request.AlimentoId,
+                UsuarioId= id,
+                Status=true
+            };
+
+            _context.Alergias.Add(entity);
+            await _context.SaveChangesAsync();
+
+            HttpContext.Response.Headers.Add("location", $"/api/usuario/{entity.Id}*");
+
+            return Ok();
+        }
+
+
+        [HttpDelete("Eliminar Cuenta/{id:int}")]
+        public async Task<ActionResult<Alergia>> Delete(int id)
+        {
+            var entity = await _context.Alergias.FindAsync(id);
+            if (entity == null)
+            {
+                return NotFound("No se encontró el usuario");
+            }
+            else
+            {
+                _context.Alergias.Remove(entity);
+                _context.SaveChanges();
+                return Ok();
+            }
+
+            return Ok(entity);
+        }
+
+
+
+
+
+    }
+}
---
+++ LosFitness.API/Controllers/AlimentoControler.cs
@@ -0,0 +1,102 @@
+﻿using LosFitness.DataAccess;
+using LosFitness.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using static LosFitness.Dto.Request.BaseResponse;
+
+namespace LosFitness.API.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class AlimentoControler : ControllerBase
+    {
+        private readonly LosFitnessDbContext _context;
+
+        public AlimentoControler(LosFitnessDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<BaseResponseGeneric<ICollection<Alimento>>>> Get()
+        {
+            var response = new BaseResponseGeneric<ICollection<Alimento>>();
+
+            try
+            {
+                response.Result = await _context.Alimentos.ToListAsync();
+                response.Success = true;
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                response.Errors.Add(ex.Message);
+                return response;
+
+            }
+
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Alimento>> Get(int id)
+        {
+            var entity = await _context.Alimentos.FindAsync(id);
+            if (entity == null)
+            {
+                return NotFound("No se encontró el registro");
+            }
+
+            return Ok(entity);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Dto.Request.DtoAlimento request)
+        {
+            var entity = new Alimento
+            {
+                Nombre = request.Nombre,
+                 Status=true,
+            };
+
+            _context.Alimentos.Add(entity);
+            await _context.SaveChangesAsync();
+
+            HttpContext.Response.Headers.Add("location", $"/api/alimento/{entity.Id}*");
+
+            return Ok();
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> Put(int id, Dto.Request.DtoAlimento request)
+        {
+            var entity = await _context.Alimentos.FindAsync(id);
+
+            if (entity == null) return NotFound();
+
+            entity.Nombre = request.Nombre;
+           
+            _context.Entry(entity).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return Ok(new { Id = id });
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult<Alimento>> Delete(int id)
+        {
+            var entity = await _context.Alimentos.FindAsync(id);
+            if (entity == null)
+            {
+                return NotFound("No se encontró el registro");
+            }
+            else
+            {
+                _context.Alimentos.Remove(entity);
+                _context.SaveChanges();
+                return Ok();
+            }
+
+            return Ok(entity);
+        }
+    }
+}
