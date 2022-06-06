a8064d2284c2295e9c2b9058de7ababfd52deef3
Nicola-Rov <102605640+Nicola-Rov@users.noreply.github.com>
2022-06-05 20:30:56 -0500

    Add files via upload

--- LosFitness.DataAccess/LosFitnessDbContext.cs
+++ LosFitness.DataAccess/LosFitnessDbContext.cs
@@ -16,7 +16,7 @@ public class LosFitnessDbContext : DbContext
             
     }
 
-    public DbSet<Actividad> Actividads { get; set; }
+    public DbSet<Actividad> Actividades { get; set; }
     public DbSet<Alergia> Alergias { get; set; }
     public DbSet<Alimento> Alimentos { get; set; }
     public DbSet<Alimento_Dieta> Alimento_Dietas { get; set; }
@@ -24,12 +24,11 @@ public class LosFitnessDbContext : DbContext
     public DbSet<Comentario> Comentarios { get; set; }
     public DbSet<Dieta> Dietas { get; set; }
     public DbSet<Horario> Horarios { get; set; }
-    public DbSet<Horario_Actividad> Horario_Actividads { get; set; }
+    public DbSet<Horario_Actividad> Horario_Actividades { get; set; }
     public DbSet<Horario_Objetivo> Horario_Objetivos { get; set; }
     public DbSet<Objetivo> Objetivos { get; set; }
     public DbSet<Recompensa> Recompensas { get; set; }
     public DbSet<Recompesa_Usuario> Recompesa_Usuarios { get; set; }
     public DbSet<Plan> Plans { get; set; }
     public DbSet<Usuario> Usuarios { get; set; }
-
 }
