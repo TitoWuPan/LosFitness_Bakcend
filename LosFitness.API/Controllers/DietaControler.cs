15a028431b38f4ed967b23b9905cfcfd35858b41
QuispeAyalaDiego <89178039+QuispeAyalaDiego@users.noreply.github.com>
2022-06-20 21:48:32 -0500

    Add files via upload

--- LosFitness.API/Controllers/DietaControler.cs
+++ LosFitness.API/Controllers/DietaControler.cs
@@ -2,82 +2,60 @@
 using LosFitness.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using static LosFitness.Dto.Request.BaseResponse;
+
+
 namespace LosFitness.API.Controllers
 {
 
+
+
+
+
     [ApiController]
     [Route("api/[Controller]")]
-
-    public class DietaControler:ControllerBase
+    public class DietaControler : ControllerBase
     {
 
+
+
         private readonly LosFitnessDbContext _context;
+
+
+
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Dto.Request.DtoDieta request)
+        {
+            var entity = new Dieta
+            {
+                Titulo = request.Titulo,
+                Describcion = request.Describcion,
+                Puntuacion = request.Puntuacion,
+                CatalogoId = request.CatalogoId,
+
+            };
+
+            _context.Dietas.Add(entity);
+            await _context.SaveChangesAsync();
+
+            HttpContext.Response.Headers.Add("location", $"/api/Dieta/{entity.Id}*");
+
+            return Ok();
+        }
+
 
         public DietaControler(LosFitnessDbContext context)
         {
             _context = context;
         }
 
-        [HttpGet]
-        public async Task<ActionResult<BaseResponseGeneric<ICollection<Dieta>>>> Get()
-        {
-            var response = new BaseResponseGeneric<ICollection<Dieta>>();
-
-            try
-            {
-                response.Result = await _context.Dietas.ToListAsync();
-                response.Success = true;
-                return Ok(response);
-            }
-            catch (Exception ex)
-            {
-                response.Errors.Add(ex.Message);
-                return response;
-
-            }
-
-        }
-
-        [HttpGet("{id:int}")]
-        public async Task<ActionResult<Dieta>> Get(int id)
+        [HttpDelete("Eliminar Dieta/{id:int}")]
+        public async Task<ActionResult<Usuario>> Delete(int id)
         {
             var entity = await _context.Dietas.FindAsync(id);
             if (entity == null)
             {
-                return NotFound("No se encontró el registro");
-            }
-
-            return Ok(entity);
-        }
-
-        [HttpPost("Registro")]
-        public async Task<ActionResult> Post(Dto.Request.DtoDieta request)
-        {
-            var entity = new Dieta
-            {
-                Titulo =request.Titulo,
-                Describcion = request.Describcion,
-                Puntuacion=request.Puntuacion,
-                CatalogoId=request.CatalogoId,
-            };
-
-            _context.Dietas.Add(entity);
-            await _context.SaveChangesAsync();
-
-            HttpContext.Response.Headers.Add("location", $"/api/usuario/{entity.Id}*");
-
-            return Ok();
-        }
-
-  
-        [HttpDelete("Eliminar Cuenta/{id:int}")]
-        public async Task<ActionResult<Dieta>> Delete(int id)
-        {
-            var entity = await _context.Dietas.FindAsync(id);
-            if (entity == null)
-            {
-                return NotFound("No se encontró el usuario");
+                return NotFound("No se encontró la dieta");
             }
             else
             {
@@ -88,9 +66,5 @@ namespace LosFitness.API.Controllers
 
             return Ok(entity);
         }
-
-
-
-
     }
 }
